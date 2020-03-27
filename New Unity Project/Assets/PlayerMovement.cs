286069e74ab66ba68681f2b3fde9c27a5298cd7c
Didac Cerver <didaccerver@gmail.com>
2020-03-27 17:19:47 +0100

    Add Unity project

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+
+    // Update is called once per frame
+    void FixedUpdate() {
+	rigidbody.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
