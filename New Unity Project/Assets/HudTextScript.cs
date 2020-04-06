357dcf364331605d0ec2a390b2b1dce2bdca5ca5
Didac Cerver <didaccerver@gmail.com>
2020-04-06 16:12:45 +0200

    Add fonts, HUD and HUD's script

---
+++ New Unity Project/Assets/HudTextScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudTextScript : MonoBehaviour
+{
+    public Transform playerPosition;
+    public Text hudText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        string hud = "Position: " + playerPosition.position.z.ToString("0") + "\n" +
+            "HP: " + "\n" +
+            "Ammo: ";
+        hudText.text = hud;
+        //hudText.text = playerPosition.position.z.ToString("0");
+    }
+}
