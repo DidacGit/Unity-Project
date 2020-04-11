9883ff780f6d84cd8bf3902697a4764146d8557b
Puit <joseppuit@hotmail.com>
2020-04-11 13:02:06 +0200

    Guardamos las scores en scores.xml, falta adaptarlo adecuadamente a las escenas

--- New Unity Project/Assets/Scripts/UserSceneManager.cs
+++ New Unity Project/Assets/Scripts/UserSceneManager.cs
@@ -4,35 +4,54 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using System.IO;
 
 public class UserSceneManager : MonoBehaviour
 {
 
-    public PlayerMovement player;
-    public PlayerBox playerBox;
-    public EnemyCreator enemyCreator;
-    public PauseMenuScript menusManager;
-    public Text textOnStart;
-    public bool started = false;
-    public bool gameOver = false;
-    public float timeGame= 0f;
-    public float finalTime = 90f;
-    public Transform camera;
-    public MeshCollider colliderPlayer;
-    public GameObject wonMenu;
-    public TMPro.TextMeshProUGUI wonMenuScore;
-    public GameObject wonSound;
-    public GameObject pauseMusic;
-    public AudioSource muteMusic;
+    public PlayerMovement player;                           //referencia a nuestro jugador
+    public PlayerBox playerBox;                             //Referencia al playerBox de la escena
+    public EnemyCreator enemyCreator;                       //Referencia al creador de enemigos situado en la estrella de la muerte
+    public PauseMenuScript menusManager;                    //Referencia al gestor de menus
+    public Text textOnStart;                                //Texto que nos pide que toquemos la pantalla al empezar el juego
+    public bool started = false;                            //Variable  que nos indicará si el juego a empezado (una vez tocada la pantalla)
+    public bool gameOver = false;                           //Variable que nos indicará si nos hemos quedado sin vida
+    public float timeGame= 0f;                              //Variable que nos ayuda a contar el tiempo y compararlo con finalTime
+    public float finalTime = 90f;                           //variable para ajustar cuanto dura el nivel, devbe ser mayor que 5
+    public Transform camera;                                //Referencia a la camara, para quitarla del parent en la animación final
+    public MeshCollider colliderPlayer;                     //Referencia del collider del player para desactivarlo en la animación final
+    public GameObject wonMenu;                              //Referencia al menú que se desplegará al ganar
+    public TMPro.TextMeshProUGUI wonMenuScore;              //Referencia al wonMenuscore que se desplegará en caso de que el usuario quiera ver las scores
+    public GameObject wonSound;                             //Prefab que accionará el sonido de victoria
+    public GameObject pauseMusic;                           //Prefab que accionará el sonido de pausa
+    public AudioSource muteMusic;                           //Variable para parar la musica
+    
+    bool backGroundMusicMute = false;                       //Variable que gestiona si los sonidos están muteados
 
-    bool a = false;
+    public Text textComprobacion;                           //Texto utilizado para comprobar que se leen los ficheros xml
 
-
-
-    private bool ending = false;
+    private bool ending = false;                            //Activa la animación final cuando esta acabando el nivel
+    ScoresCollection scoresCollection;                      //Variable para cargar y guardar las scores en un fichero XML
+    string rootFolder;                                      //Path donde se guardarán las scores
+    bool gameSaved = false;                                 //Variable necesaria para que no guarde la nueva score a cada frame
     private void Start()
     {
+        //Funcionan perfectamente pero lo guardan en la carpeta raiz "New Unity Project" y no se verá desde android
+        //scoreCollection.Save("scores.xml"); 
+        //ScoresCollection scoreLoad = ScoresCollection.Load("scores.xml");
+
+        //Creamos una variable para poner el path
         
+        //Si estamos desde el editor de Unity cogera el path de la aplicación
+#if UNITY_EDITOR
+        rootFolder = Application.dataPath;
+        //Si estamos en Android o iOS cogerá el path de persistentDataPath, que es una carpeta que crea unity en estos dispositivos con
+        //los archivos que necesitará mas tarde como es el caso
+#elif UNITY_ANDROID || UNITY_IOS
+        rootFolder = Application.persistentDataPath;
+#endif
+
+        scoresCollection = ScoresCollection.Load(Path.Combine(rootFolder, "scores.xml"));
         Time.timeScale = 0f;
        
     }
@@ -60,14 +79,29 @@ public class UserSceneManager : MonoBehaviour
             wonMenu.SetActive(true);
             wonMenuScore.text = "SCORE: " + player.score.ToString("0");
             Time.timeScale = 0f;
-            if (a == false) {
+
+            //Si aun no hemos guardado, lo hará pero solo una vez
+            if (!gameSaved)
+            {
+                //Creamos una Score
+                Score myScore = new Score("user2", player.score);
+                //La añadimos a scoresCollection
+                scoresCollection.scores.Add(myScore);
+                //Lo guardamos en la ruta RootFolder y en el fichero scores.xml
+                scoresCollection.Save(Path.Combine(rootFolder, "scores.xml"));
+                //Lo representamos en textComprobación <<<<<<<<<<<<<<<<<<<<BORRAR>>>>>>>>>>>>>>>>>>>>>
+                textComprobacion.text = scoresCollection.ToString();
+                //Indicamos que ya se ha guardado para que en el siguiente frame no lo vuelva a hacer
+                gameSaved = true;
+            }
+            
+
+            if (backGroundMusicMute == false) {
                 muteMusic.mute = true;
                 Instantiate(pauseMusic);
-                a = true;
+                backGroundMusicMute = true;
             }
-          
         }
-            
     }
 
     private void FinalAnimation()
